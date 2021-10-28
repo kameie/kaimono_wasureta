d6df7b05c195ea3cbaad8fd3458ae8dd6eac33b2
kameie <87402102+kameie@users.noreply.github.com>
2021-10-28 17:35:43 +0900

    メモを作成した本人にしか見えないようにする

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 <div class="col-md-6 mx-auto">
     買い物メモに商品名と使い切るまでのおおよその日数を入力してメモを作りましょう！
-    @if( Auth::check() )
+    
     <a class="btn btn-primary btn-md" href="{{ route('item.create') }}">
         <i class="far fa-create"></i>＋買い物メモを作る
     </a>  
     @foreach ($items as $item)
+    @if ($item->user_id === Auth::id())
         <div class="card-wrap">
             <div class="card mt-3">
                 <div class="card-header">
@@ -23,13 +24,14 @@
                         <a class="btn btn-danger btn-sm" rel="nofollow" href="{{ route('item.delete', ['id' => $item->id]) }}">
                             <i class="far fa-trash-alt"></i>削除
                         </a>
-                        
+                         
                     </div>
                 </div>
             </div>   
         </div>
+    @endif
     @endforeach
-@endif
+
 
 </div>
 @endsection
