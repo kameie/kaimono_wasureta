e733bbb3d3ac74f0f6ef13cb65603820834cb544
kameie <87402102+kameie@users.noreply.github.com>
2021-11-23 23:39:08 +0900

    インデントの修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,41 +1,64 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="text-center my-2">{{ __('ログイン') }}</h4></div>
+                    <h4 class="text-center my-2">{{ __("ログイン") }}</h4>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
+                            <label
+                                for="email"
+                                class="col-md-4 col-form-label text-md-right"
+                                >{{ __("メールアドレス") }}</label
+                            >
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    required
+                                    autocomplete="email"
+                                    autofocus
+                                />
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
+                            <label
+                                for="password"
+                                class="col-md-4 col-form-label text-md-right"
+                                >{{ __("パスワード") }}</label
+                            >
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input
+                                    id="password"
+                                    type="password"
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    name="password"
+                                    required
+                                    autocomplete="current-password"
+                                />
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -43,12 +66,16 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-md">
-                                    {{ __('ログインする') }}
-                                </button>                                
+                                    {{ __("ログインする") }}
+                                </button>
                             </div>
                             <div class="col text-center mt-2">
-                <a class="btnorigin btn-outline-secondary" href="{{ route('item.index') }}">戻る</a>
-                </div>
+                                <a
+                                    class="btnorigin btn-outline-secondary"
+                                    href="{{ route('item.index') }}"
+                                    >戻る</a
+                                >
+                            </div>
                         </div>
                     </form>
                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,88 +1,147 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="text-center my-2">{{ __('ユーザー登録') }}</h4>
+                    <h4 class="text-center my-2">{{ __("ユーザー登録") }}</h4>
                 </div>
-    
+
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <p class="col-md-12 text-center"><span class="text-danger">(※)</span>は入力必須項目です。</p>
+                            <p class="col-md-12 text-center">
+                                <span class="text-danger">(※)</span
+                                >は入力必須項目です。
+                            </p>
                         </div>
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('名前') }}<span class="text-danger">(※)</span></label>
+                            <label
+                                for="name"
+                                class="col-md-4 col-form-label text-md-right"
+                                >{{ __("名前")
+                                }}<span class="text-danger">(※)</span></label
+                            >
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input
+                                    id="name"
+                                    type="text"
+                                    class="form-control @error('name') is-invalid @enderror"
+                                    name="name"
+                                    value="{{ old('name') }}"
+                                    required
+                                    autocomplete="name"
+                                    autofocus
+                                />
                                 <small>30文字以内で入力して下さい。</small>
 
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}<span
-                                    class="text-danger">(※)</span></label>
+                            <label
+                                for="email"
+                                class="col-md-4 col-form-label text-md-right"
+                                >{{ __("メールアドレス")
+                                }}<span class="text-danger">(※)</span></label
+                            >
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                <small>今回は仮のメールアドレスを入力して下さい。</small>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    required
+                                    autocomplete="email"
+                                />
+                                <small
+                                    >今回は仮のメールアドレスを入力して下さい。</small
+                                >
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}<span
-                                    class="text-danger">(※)</span></label>
+                            <label
+                                for="password"
+                                class="col-md-4 col-form-label text-md-right"
+                                >{{ __("パスワード")
+                                }}<span class="text-danger">(※)</span></label
+                            >
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                <small>半角英数字8文字以上を入力して下さい。</small>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    name="password"
+                                    required
+                                    autocomplete="new-password"
+                                />
+                                <small
+                                    >半角英数字8文字以上を入力して下さい。</small
+                                >
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('パスワード(確認用)') }}<span
-                                    class="text-danger">(※)</span></label>
+                            <label
+                                for="password-confirm"
+                                class="col-md-4 col-form-label text-md-right"
+                                >{{ __("パスワード(確認用)")
+                                }}<span class="text-danger">(※)</span></label
+                            >
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                <small>確認のためパスワードを再度入力して下さい。</small>
+                                <input
+                                    id="password-confirm"
+                                    type="password"
+                                    class="form-control"
+                                    name="password_confirmation"
+                                    required
+                                    autocomplete="new-password"
+                                />
+                                <small
+                                    >確認のためパスワードを再度入力して下さい。</small
+                                >
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-md">
-                                    {{ __('新規登録する') }}
+                                    {{ __("新規登録する") }}
                                 </button>
                             </div>
                             <div class="col text-center mt-3">
-                <a class="btnorigin btn-outline-secondary" href="{{ route('item.index') }}">戻る</a>
-                </div>
+                                <a
+                                    class="btnorigin btn-outline-secondary"
+                                    href="{{ route('item.index') }}"
+                                    >戻る</a
+                                >
+                            </div>
                         </div>
                     </form>
                 </div>
